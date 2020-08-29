e14a6711de65a09eeac1fafabf9dee860818e03a
Pedro Gabriel <gaboleal123@gmail.com>
2020-08-29 12:51:47 -0400

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => Str::'administrador',
+            'email' => Str::'admin@gmail.com',
+            'password' => bcrypt('admin123'),
+        ]);
     }
 }
